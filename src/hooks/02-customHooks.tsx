6ec6d238d31204243f6834cc96ecc96f578b27cd
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-13 18:10:18 +0200

    117

--- src/hooks/02-customHooks.tsx
+++ src/hooks/02-customHooks.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react"
 
 const useCounter = (value:number = 10) => {
-    const [ counter , setCounter ] = useState(value);
-    return{counter}
+    const [ counter , setCounter ] = useState<number>(value);
+    const sc = (arg:string):void => {
+        switch(arg){
+            case '+': setCounter(counter+1);break;
+            case '-': setCounter(counter-1);break;
+            case 'R': setCounter(value);break;
+        }
+    };
+    return{counter,sc}
 }
 
 export const useCustomHook = () => {
 
-    const { counter } = useCounter();
+    const { counter , sc } = useCounter();
 
     return(
         <>
@@ -15,9 +22,9 @@ export const useCustomHook = () => {
 
         <code>{counter}</code>
 
-        <button className="btn btn-primary" >+1</button>
-        <button className="btn btn-primary" >-1</button>
-        <button className="btn btn-primary" >RESET</button>
+        <button className="btn btn-primary" onClick={() => {sc('+')}} >+1</button>
+        <button className="btn btn-primary" onClick={() => {sc('-')}} >-1</button>
+        <button className="btn btn-primary" onClick={() => {sc('R')}} >RESET</button>
 
         </>
     )
