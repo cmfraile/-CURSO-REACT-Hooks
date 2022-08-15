4034a19d38cd2b0b5a3284043810df9c2e3a8746
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-15 16:44:23 +0200

    133 use callback con argumentos

--- src/hooks/10-callbackhook.tsx
+++ src/hooks/10-callbackhook.tsx
@@ -1,8 +1,8 @@
 import { useCallback, useState, memo } from "react";
 
-const ShowIncrement = memo(({increment}:{increment:() => void}) => {
+const ShowIncrement = memo(({increment}:{increment:(value:number) => void}) => {
     console.log('redibujado del callbackhook');
-    return(<button className="btn btn-primary" onClick={increment}>+1</button>)
+    return(<button className="btn btn-primary" onClick={() => {increment(5)}}>+1</button>)
 })
 
 export const callbackhook = () => {
@@ -10,7 +10,7 @@ export const callbackhook = () => {
     //recuerda que el dispatcher puede tener un callback que recoje en su argumento el calor actual. De este modo, no generas una nueva función dentro del use Callback y no pierdes optimización:
 
     const [ counter , setCounter ] = useState(0);
-    const increment = useCallback(() => { setCounter((v) => v+1) },[]);
+    const increment = useCallback((value:number) => { setCounter((c) => c+value) },[]);
     
     
     return(
