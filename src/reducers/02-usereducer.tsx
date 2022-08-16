a813f66ff8d3be8876732bd851ae3b535d4c6133
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-16 20:16:40 +0200

    143

--- src/reducers/02-usereducer.tsx
+++ src/reducers/02-usereducer.tsx
@@ -1,13 +1,25 @@
 import { useReducer } from "react"
-import './todo.sass'
+import './todo.sass';
 
-const todocraft = (todo:string) => {return {id:new Date().getTime(),todo,done:false}};
-const iState:{id:number,todo:string,done:boolean}[] = [todocraft('Estado génesis')];
-const todoReducer = (initialState:{id:number,todo:string,done:boolean}[]|[] = [],action:{type:string,payload?:{id:number,todo:string,done:boolean}}) => {
+interface initialState {id:number,todo:string,done:boolean} ; interface todobj {id:number,todo:string,done:boolean} ;
+
+const todoReducer = (initialState:initialState = [todocraft('Estado génesis')],action:{type:string,payload?:initialState}) => {
     if(!action.payload){return initialState}
     switch(action.type){
         default:break;
     }
+}
+const todocraft = (todo:string):todobj => {return {id:new Date().getTime(),todo,done:false}};
+const initialState:initialState|[] = [todocraft('Estado génesis')];
+
+
+const TodoItem = ({todos}:{todos:{id:number,todo:string,done:boolean}[]}):JSX.Element => {
+
+    return(
+        <>
+        <ul>{todos.map( ({id,todo}) => {return <li className="litem" key={id}>{todo} <button className="btn btn-danger"></button></li>})}</ul>
+        </>
+    )
 }
 
 export const TodoApp = () => {
@@ -20,8 +32,7 @@ export const TodoApp = () => {
         <div className="container">
             <div className="row">
                 <div className="col">
-                    {/* TodoItem */}
-                    {(todos) && (<ul>{todos.map( ({id,todo}) => {return <li className="litem" key={id}>{todo} <button className="btn btn-danger"></button></li>})}</ul>)}
+                    <TodoItem todos={todos}/>
                 </div>
                 <div className="col">
                     {/*TodoAdd y onNewTodo*/}
