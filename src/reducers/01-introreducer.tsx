e9197322c40ff897c9ddab42074ec39182657bc4
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-16 17:52:23 +0200

    140

--- src/reducers/01-introreducer.tsx
+++ src/reducers/01-introreducer.tsx
@@ -1,7 +1,15 @@
 export const Introreducer = () => {
-    return (
-        <>
-        <h1>Intro reducer</h1>
-        </>
-    )
+    const todoReducer = (state = [{id:1,todo:'recolectar olivas',done:false}] , action?:any) => {
+        if(!action){return state};
+        switch(action.type){
+            case '[ TODO ] addtodo' : return [...state,action.payload] ;
+            default:return state;
+        }
+    }
+    let todos = todoReducer();
+    todos = todoReducer(todos,{type:'[ TODO ] addtodo',payload:{id:2,todo:'recolectar asitunas',done:false}});
+    todos = todoReducer(todos,{type:'[ TODO ] addtodo',payload:{id:3,todo:'recolectar patatas',done:false}});
+    console.log(todos);
+
+    return (<><h2>Intro reducer</h2></>)
 }
