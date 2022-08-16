d85408b9895f6ea393e0b48a7ba531375d80a926
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-16 19:31:24 +0200

    142

--- src/reducers/02-usereducer.tsx
+++ src/reducers/02-usereducer.tsx
@@ -1,7 +1,8 @@
 import { useReducer } from "react"
+import './todo.sass'
 
 const todocraft = (todo:string) => {return {id:new Date().getTime(),todo,done:false}};
-const iState:{id:number,todo:string,done:boolean}[]|[] = [todocraft('Estado génesis')];
+const iState:{id:number,todo:string,done:boolean}[] = [todocraft('Estado génesis')];
 
 const todoReducer = (initialState:{id:number,todo:string,done:boolean}[]|[] = [],action:{type:string,payload?:{id:number,todo:string,done:boolean}}) => {
     if(!action.payload){return initialState}
@@ -17,6 +18,20 @@ export const TodoApp = () => {
     return (
         <>
         <h2>Todo app</h2>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    {(todos) && (<ul>{todos.map( ({id,todo}) => {return <li className="litem" key={id}>{todo} <button className="btn btn-danger"></button></li>})}</ul>)}
+                </div>
+                <div className="col">
+                    <input      type="text"
+                                className="form-control iform"
+                                placeholder="nueva tarea"
+                                name="newtodo"
+                    />
+                </div>
+            </div>
+        </div>
         </>
     )
 }
