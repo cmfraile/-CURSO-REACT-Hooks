a3a6c412cd3e0960451ea18dc51b2802d60bdf1d
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-20 10:56:32 +0200

    157 pretarea

--- src/useContext/mainContext.tsx
+++ src/useContext/mainContext.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate , Link } from 'react-router-dom'
 import { about as About } from '.';
 import { login as Login } from '.';
 import './useContext.sass'
@@ -8,6 +8,9 @@ export const mainContext = () => {
     return(
         <>
             <h1>Main</h1>
+            {/* La diferencia entre a href y Link es que Link no actualiza toda la web, manteniendo el estado: */}
+            <Link to='/'>About</Link>
+            <Link to='/login'>Login</Link>
             <hr />
             <Routes>
                 <Route path='/' element={<About />} ></Route>
