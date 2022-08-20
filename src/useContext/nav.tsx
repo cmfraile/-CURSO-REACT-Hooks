d3340ed0d184253b0f0ab61cd4a05b53207a02aa
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-20 11:18:33 +0200

    158

--- src/useContext/nav.tsx
+++ src/useContext/nav.tsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link , NavLink } from "react-router-dom"
 
 export const Nav = () => {
 
+    {/*return(<><Link to='/'>About</Link><Link to='/login'>Login</Link></>)*/}
+
     return(
         <>
-            <Link to='/'>About</Link>
-            <Link to='/login'>Login</Link>
+        <NavLink    to='/'> About </NavLink>
+        <NavLink    to='/login'
+                    className={(ia) => {console.log(ia);return ''}}
+                    style={(ia) => {console.log(ia);return {color:'blue'}}}
+                    > Login </NavLink>
         </>
     )
 }
