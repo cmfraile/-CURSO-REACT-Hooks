5c5def58bb94a2116c444dedc7040f478a26a5f0
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-22 16:52:59 +0200

    167

--- testing/hooks/useForma.test.ts
+++ testing/hooks/useForma.test.ts
@@ -14,16 +14,22 @@ const main = () => {
         });
     });
 
-    test('debe de cambiar el nombre del formulario',() => {
+    test('Comprobemos que funcionen los eventos del formulario',() => {
         //montar el hook,llamar el oninputchange con act,el evento, y el expect (result.current.name y formstate.name === juan);
         const iform = {name:'Carlos',email:'carlos@fraile.com'} ; const nuevonombre = "Juan"
         const rHOOK = renderHook(() => useForma(iform));
-        const { onInputChange } = rHOOK.result.current;
+        const { onInputChange , onResetForm } = rHOOK.result.current;
         act(() => {onInputChange({target:{name:'name',value:nuevonombre}})});
         expect(rHOOK.result.current).toEqual({
             name:nuevonombre,email:iform.email,formState:{name:'Juan',email:iform.email},
             onInputChange:expect.any(Function),
             onResetForm:expect.any(Function),
+        });
+        act(() => {onResetForm()});
+        expect(rHOOK.result.current).toEqual({
+            name:iform.name,email:iform.email,formState:iform,
+            onInputChange:expect.any(Function),
+            onResetForm:expect.any(Function)
         })
     });
 
