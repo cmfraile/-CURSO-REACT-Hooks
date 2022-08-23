8e9c6dab8ed1490dd3fe84e5a1b660bc1f195a7e
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-23 19:44:33 +0200

    174

--- testing/todoTAREA/Apptodo.test.tsx
+++ testing/todoTAREA/Apptodo.test.tsx
@@ -4,11 +4,16 @@ import { render , screen } from '@testing-library/react';
 
 const main = () => {
 
+    /*Pruebas que hace el profe sin usar context:
+        - Renderizar correctamente.
+        - 
+    */
+
     test('snapshot',() => {
         expect(render(<TodoApp/>).container).toMatchSnapshot();
         screen.debug();
     });
-    
+
 }
 
 describe('pruebas sobre el <AppTodo>',main);
