1ca3b0bf3a95a2906cc366206a3e475c11ff4d09
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-24 15:38:47 +0200

    176 y nos quedamos con dudas

--- testing/todoTAREA/micontext.test.tsx
+++ testing/todoTAREA/micontext.test.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { TodoApp } from '../../src/todocontexttarea/maintodo';
-import { todoProvider as TDP } from "../../src/todocontexttarea/context/todoContext";
-import { screen , render } from '@testing-library/react';
+import { todoContext as TDC } from "../../src/todocontexttarea/context/todoContext";
+import { todocrudch } from "../../src/todocontexttarea/hooks/todocrudhook";
+import { screen , render, renderHook } from '@testing-library/react';
 
 JSON.parse = jest.fn().mockImplementationOnce(() => []);
 
 const main = () => {
 
-    test('snapshot:',() => {
-        expect(render(<TDP><TodoApp/></TDP>).container).toMatchSnapshot();
-    });
+    beforeEach(() => {jest.clearAllMocks()});
+
+    test('snapshot:',() => {expect(render(<TDC.Provider value={{...todocrudch()}}><TodoApp/></TDC.Provider>).container).toMatchSnapshot()});
+    test('debe de introducir un valor en el todo',() => {
+
+    })
 
     
 
